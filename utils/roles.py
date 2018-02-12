ac9f7c9ec6b8d27e5a61cd6c78cda95ad8a4c648
Marie-Donnie <delavergnemarie@gmail.com>
2018-02-12 16:29:27 +0100

    Removed useless spaces

--- utils/roles.py
+++ utils/roles.py
@@ -4,16 +4,16 @@ from enoslib.host import Host
 def to_enos_roles(roles):
     """Transform the roles to use enoslib.host.Host hosts
     instead of dict
-    
+
     :param roles: roles returned by deploy5k
     """
 
     def to_host(h):
         extra = {}
         # create extra_vars for the nics
-        # network_role = ethX 
+        # network_role = ethX
         for nic, roles in h["nics"]:
-            for role in roles: 
+            for role in roles:
                 extra[role] = nic
         return Host(h["host"], user="root", extra=extra)
 
@@ -21,4 +21,3 @@ def to_enos_roles(roles):
     for role, hosts in roles.items():
         enos_roles[role] = [to_host(h) for h in hosts]
     return enos_roles
-
