75b80944fe6d7493f05e262425692a525e5e5562
Marie-Donnie <delavergnemarie@gmail.com>
2018-04-23 17:21:08 +0200

    Some flycheck corrections

--- experiments/read-write-ratio.py
+++ experiments/read-write-ratio.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.insert(0, '..') # Give me an access to juice
 
 import logging
 import os
-from pprint import pformat, pprint
+from pprint import pprint
 
 import pymysql
 import yaml
 
 import juice as j
+
+
+sys.path.insert(0, '..')  # Give me an access to juice
 
 # This experiment runs Keystone rally scenarios referred in
 # `SCENARIOS` and gives the database reads/write ratio for each. Note
@@ -118,7 +120,7 @@ CONF = {
 }
 
 SCENARIOS = [
-    "keystone/authenticate-user-and-validate-token.yaml"
+  "keystone/authenticate-user-and-validate-token.yaml"
   , "keystone/create-add-and-list-user-roles.yaml"
   , "keystone/create-and-list-tenants.yaml"
   , "keystone/get-entities.yaml"
@@ -133,88 +135,91 @@ logging.basicConfig(level=logging.INFO)
 
 
 def init():
-  try:
-    j.deploy(CONF, 'mariadb', False, JOB_NAME)
-    j.openstack('mariadb', env=JOB_NAME)
-  except Exception as e:
-    logging.error("Setup goes wrong with error: %s" % e)
+    try:
+        j.deploy(CONF, 'mariadb', False, JOB_NAME)
+        j.openstack('mariadb', env=JOB_NAME)
+    except Exception as e:
+        logging.error("Setup goes wrong with error: %s" % e)
 
 
 def total_reads_writes(host_address):
-  """Returns the total of reads and writes on `host_address`.
+    """Returns the total of reads and writes on `host_address`.
 
-  Returns a tuple with first, the total of reads, and second, the
-  total of writes that have been performed on the mariadb database
-  `host_address`.
-  """
-  SQL = '''
-  SELECT
-    SUM(IF(variable_name = 'Com_select', variable_value, 0))
-       AS `Total reads`,
-    SUM(IF(variable_name IN ('Com_delete',
-                             'Com_insert',
-                             'Com_update',
-                             'Com_replace'), variable_value, 0))
-       AS `Total writes`
-  FROM  information_schema.GLOBAL_STATUS;
-'''
-  result = (0, 0)
-  db_conn = pymysql.connect(host=host_address,
-                            user='root',
-                            password='my-secret-pw',
-                            db='information_schema',
-                            cursorclass=pymysql.cursors.DictCursor)
+    Returns a tuple with first, the total of reads, and second, the
+    total of writes that have been performed on the mariadb database
+    `host_address`.
+    """
+    SQL = '''
+    SELECT
+      SUM(IF(variable_name = 'Com_select', variable_value, 0))
+         AS `Total reads`,
+      SUM(IF(variable_name IN ('Com_delete',
+                               'Com_insert',
+                               'Com_update',
+                               'Com_replace'), variable_value, 0))
+         AS `Total writes`
+    FROM  information_schema.GLOBAL_STATUS;
+  '''
+    result = (0, 0)
+    db_conn = pymysql.connect(host=host_address,
+                              user='root',
+                              password='my-secret-pw',
+                              db='information_schema',
+                              cursorclass=pymysql.cursors.DictCursor)
 
-  try:
-    with db_conn.cursor() as cursor:
-      cursor.execute(SQL)
-      sql_res = cursor.fetchone()
-      logging.info("Read/Write for database %s: %s" % (db_conn, sql_res))
+    try:
+        with db_conn.cursor() as cursor:
+            cursor.execute(SQL)
+            sql_res = cursor.fetchone()
+            logging.info("Read/Write for database %s: %s" % (db_conn, sql_res))
 
-      result = (sql_res.get('Total reads', 0), sql_res.get('Total writes', 0))
-  except Exception as e:
-    logging.error("Error while requesting database: %s" % e)
-  finally:
-    db_conn.close()
+            result = (sql_res.get('Total reads', 0),
+                      sql_res.get('Total writes', 0))
+    except Exception as e:
+        logging.error("Error while requesting database: %s" % e)
+    finally:
+        db_conn.close()
 
-  return result
+    return result
+
 
 def read_write_ratio():
-  results = {}
-  db_host = None
+    results = {}
+    db_host = None
 
-  # Get the address of the database
-  with open(os.path.join(JOB_NAME, 'env'), "r") as f:
-    db_host = yaml.load(f)['roles']['database'][0]
-    logging.info("Database is %s", db_host)
+    # Get the address of the database
+    with open(os.path.join(JOB_NAME, 'env'), "r") as f:
+        db_host = yaml.load(f)['roles']['database'][0]
+        logging.info("Database is %s", db_host)
 
-  for scn in SCENARIOS:
-    logging.info("Treating scenario: %s" % scn)
-    # Gets total reads/writes in the database; then executes rally
-    # scenario; and finally re-gets total reads/writes.
-    total_reads_before_scn, total_writes_before_scn = total_reads_writes(
-      db_host.address)
-    j.rally([scn], "keystone")
-    total_reads_after_scn, total_writes_after_scn = total_reads_writes(
-      db_host.address)
+    for scn in SCENARIOS:
+        logging.info("Treating scenario: %s" % scn)
+        # Gets total reads/writes in the database; then executes rally
+        # scenario; and finally re-gets total reads/writes.
+        total_reads_before_scn, total_writes_before_scn = total_reads_writes(
+          db_host.address)
+        j.rally([scn], "keystone")
+        total_reads_after_scn, total_writes_after_scn = total_reads_writes(
+          db_host.address)
 
-    # Compute number of reads/writes for this scenario and its ratio
-    scn_reads  = total_reads_after_scn - total_reads_before_scn
-    scn_writes = total_writes_after_scn - total_writes_before_scn
-    scn_rws    = scn_reads + scn_writes
+        # Compute number of reads/writes for this scenario and its ratio
+        scn_reads  = total_reads_after_scn - total_reads_before_scn
+        scn_writes = total_writes_after_scn - total_writes_before_scn
+        scn_rws    = scn_reads + scn_writes
 
-    results[scn] = {
-      'reads'  : scn_reads,
-      'writes' : scn_writes,
-      '%reads' : round((scn_reads / scn_rws) * 100, 2),
-      '%writes': round((scn_writes / scn_rws) * 100, 2)
-    }
+        results[scn] = {
+          'reads'  : scn_reads,
+          'writes' : scn_writes,
+          '%reads' : round((scn_reads / scn_rws) * 100, 2),
+          '%writes': round((scn_writes / scn_rws) * 100, 2)
+        }
 
-    # Log the ratio result
-    logging.info("Ratio for %s: %s" % (scn, results[scn]))
+        # Log the ratio result
+        logging.info("Ratio for %s: %s" % (scn, results[scn]))
 
-  # Output results
-  pprint(results)
+    # Output results
+    pprint(results)
+
 
 if __name__ == '__main__':
     # Do the initial reservation and boilerplate
